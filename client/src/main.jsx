de64ba5d08a38452c4e0fb2b1eef4009e07dbfc8
spriveen <rpeanuka@gmail.com>
2026-04-21 16:19:46 +0530

    update navbar.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+   throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')).render(
+   
+<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
  <BrowserRouter>
  <AppContextProvider> 
   <App />
   </AppContextProvider>
-   </BrowserRouter>,
+   </BrowserRouter>
+   </ClerkProvider>,
   
 )
